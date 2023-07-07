df695a98025d136a73bddebfd82981709da5d692
NickCross-79 <ncross7799@gmail.com>
2023-07-07 14:47:14 -0400

    Added baro location endpoint, removed console log

--- baroChecker.js
+++ baroChecker.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 const voidTrader = 'https://api.warframestat.us/pc/voidTrader';
 
+const getLocation = async () => {
+    const response = await axios.get(voidTrader);
+    return response.data.location;
+}
+
 const getInventory = async () => {
     const response = await axios.get(voidTrader);
-    console.log(response.data.activation);
     return response.data.inventory;
 }
 
-export default getInventory;
+export {
+    getLocation,
+    getInventory
+};
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import database from './database/database.js';
-import getInventory from './baroChecker.js'
+import {getInventory, getLocation} from './baroChecker.js'
 
 // Update 
 // await database.dropTable();
 // await database.createTable();
 // await database.populateTable();
 
+// Get Location
+const location = await getLocation();
+console.log(location);
+
 // Get new item
-const inventory = await getInventory();
-console.log(inventory);
+//const inventory = await getInventory();
+//console.log(inventory);
 // for(const item of inventory){
 //     if(await database.findItem(item.item) == -1)
 //         console.log("The new item this week is: ",item);
